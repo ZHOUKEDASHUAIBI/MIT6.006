f288830fdf1c20457aa7ed1095f9fc48d76c2193
Ke-Zhou <31035873+ZHOUKEDASHUAIBI@users.noreply.github.com>
2022-04-06 16:28:47 +0800

    Create InsertionSortC.cpp

---
+++ Sorting/InsertionSort/InsertionSortC.cpp
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define Size 7
+
+void InsertLast(int* A, int i);
+void InsertionSort(int* A);
+void PrintArray(int* A);
+
+int main()
+{
+	int* Array = (int*)malloc(sizeof(Size));
+	Array[0] = 5;
+	Array[1] = 2;
+	Array[2] = 7;
+	Array[3] = 1;
+	Array[4] = 4;
+	Array[5] = 8;
+	Array[6] = 6;
+	
+	PrintArray(Array);
+	InsertionSort(Array);
+	PrintArray(Array);
+	
+	return 0;
+}
+
+void InsertLast(int* A, int i)
+{	
+	int temp;
+	if(i > 0 && A[i] < A[i - 1])
+	{
+		temp = A[i];
+		A[i] = A[i - 1];
+		A[i - 1] = temp;
+		InsertLast(A, i-1);
+	}
+}
+
+void InsertionSort(int* A)
+{
+	static int i = 1;
+	if(i < Size)
+	{
+		InsertLast(A, i);
+		i++;
+		InsertionSort(A);
+	}
+}
+
+void PrintArray(int* A)
+{
+	int i = 0;
+	while(i < Size)
+	{
+		printf("%d ",A[i]);
+		i++;
+	}
+	printf("\n");
+}
